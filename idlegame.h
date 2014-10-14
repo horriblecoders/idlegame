e058426c63d458492ef60619a2d961761f36acf0
horriblecoders <horriblecoders@users.noreply.github.com>
2014-10-14 09:02:30 -0400

    Update idlegame.h
    Moved 2 functions to character.h

--- idlegame.h
+++ idlegame.h
@@ -28,8 +28,6 @@ using namespace std;
 void sleep(int seconds);
 void titleScreen(string title, string version);
 bool levelCheck(int statxp, int & statLevel);
-void buyWep(string & weapon, long & gold);
-void levelUp(int & level, int & nextLevel, string weapon, int gold);
 
 //Functions
 
@@ -48,8 +46,6 @@ bool levelCheck(int statxp, int & statLevel)
 	
 }
 
-
-
 void titleScreen(string title, string version)
 {
 	cout << title << endl;
@@ -59,31 +55,6 @@ void titleScreen(string title, string version)
 	getline(cin, input);
 	clear();
 	
-}
-
-void buyWep(string & weapon, long & gold)
-{
-	//Items
-	const string wepAdj[] = {"big","chaotic","large","small","short","strong","iron","black","white","orange","red","yellow","green","blue","purple","grey","steel","mithril","crystal","diamond","gold","silver","copper","stone","steel","floppy","sharp","brass","godly","demonic","legendary","little","ruby","emerald","sapphire","unbreakable","fragile","wood","wet","water","hot","burning","flame","thunder","lightning","cold","frozen","icy","wind","void","dark","broken","scratched","dirty","rusty","bent","dented","perfect","molten","pointy","dull","blunt","corrosive","ancient","old","rune","holy","unholy","golden","burnt","tasty","corrupted","glittering","shiny","fancy","expensive","mighty","powerful","The Magnificent","cheap","military","royal","bloody","deadly","brutal","adamantium","flawless","weak","angry","glowing","smelly","edible","phantom","slimy","sticky","jelly","moldy","poisonous","happy","sad","depressed","flat","stubby","stinky","light","heavy","invisible"};
-	const string weapons[] = {"bucket","longsword","shortsword","dagger","hatchet","battleaxe","knife","spatula","rapier","spear","scimitar","lance","javelin","halberd","trident","whip","greatsword","cutlass","club","mace","morningstar","flail","hammer","war Pick","scythe","throwing axes","throwing knives","darts","sling","slingshot","voulge","staff","stick","knuckles","shortbow","crossbow","frying pan","mallet","blowgun","shiv","hook","shovel","rake","signpost","nunchucks","shuriken","katana","falchion","flamberge","sabre","bardiche","glaive","pike","gauntlets","fish","baguette","spoon","battle stool","gunblade","tiger claws","kris","stiletto","broadsword","bastard sword","master sword","mattock","pickaxe","broadaxe","tomahawk","wrench","rock","boomerang","pitchfork","spetum","partisan","harpoon","chakram","fishing pole","scepter","lamb chop","torch","garrote","machete","hoe","fence post","chair","lute","boot","shoe","longbow","composite bow","toothpick","gloves","goblet","midget","maul","platter","pot","horn","sheers"};
-
-	weapon = wepAdj[rand()%(sizeof(wepAdj)/sizeof(string))] + " " + weapons[rand()%(sizeof(weapons)/sizeof(string))];
-	int spent = (gold * (rand()%9 * 0.1) + 1);
-	gold = gold - spent;
-	clear();
-	cout << "Bought a " << weapon << " for " << spent << " gold!" << endl;
-	sleep(2);
-}
-
-void levelUp(int & level, string weapon, int gold)
-{
-	clear();
-	level = level + 1;
-	cout << "Leveled Up!" << endl;
-	cout << "Weapon: " << weapon << endl;
-	cout << "Gold: " << gold << endl;
-	cout << "Task: Heading to the shops" << endl;
-	sleep(2);
 }
 
 void sleep(int seconds)
